f9103885abf82ec49305542179c33421faf66f80
Fajaru1503 <fajarudewangga@gmail.com>
2022-07-19 17:43:26 +0800

    remove gatsby plugin anchor links

--- gatsby-config.js
+++ gatsby-config.js
@@ -12,7 +12,6 @@ module.exports = {
     "gatsby-plugin-sharp",
     "gatsby-transformer-sharp",
     "gatsby-plugin-sass",
-    "gatsby-plugin-anchor-links",
     {
       resolve: 'gatsby-source-filesystem',
       options: {
