de5c0f6daa7c51da5a05dce0c9d7f8ca4a8eb579
Fajaru1503 <fajarudewangga@gmail.com>
2022-07-19 15:14:15 +0800

    add gatsby plugin anchor links

--- gatsby-config.js
+++ gatsby-config.js
@@ -12,6 +12,7 @@ module.exports = {
     "gatsby-plugin-sharp",
     "gatsby-transformer-sharp",
     "gatsby-plugin-sass",
+    "gatsby-plugin-anchor-links",
     {
       resolve: 'gatsby-source-filesystem',
       options: {
