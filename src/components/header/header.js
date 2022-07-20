2ca1c6e179314cf1ffc1ecfc86cd597bf7a5fb68
Fajaru1503 <fajarudewangga@gmail.com>
2022-07-20 23:07:09 +0800

    implement header design on mobile resolution

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -6,9 +6,14 @@ import "./header.scss"
 
 export const Header = ({ color, navigation }) => {
   const headerEl = useRef()
+  const navEl = useRef()
   const mobileBtnEl = useRef()
 
   const onMenuClick = (menuParam) => {
+    if (window.innerWidth < 768) {
+      resetElementsClasses()
+    }
+
     Object.keys(navigation).forEach((menu, index) => {
       if (menu === menuParam) {
         Object.values(navigation)[index]
@@ -29,12 +34,94 @@ export const Header = ({ color, navigation }) => {
 
     mobileBtnEl.current.addEventListener('click', () => {
       mobileBtnEl.current.classList.toggle('active')
+      navEl.current.classList.toggle('open')
+      document.querySelector('body').classList.toggle('overflow-hidden')
     })
   }
 
   const resetElementsClasses = () => {
     mobileBtnEl.current.classList.remove('active')
+    navEl.current.classList.remove('open')
+    document.querySelector('body').classList.remove('overflow-hidden')
   }
+
+  const renderLogo = () => (
+    <h1 className="header__logo-wrapper">
+      <a href="/" className="header__logo">
+        <img src={logo} alt="Logo" />
+      </a>
+    </h1>
+  )
+
+  const renderMenuNav = () => (
+    <>
+      <ul className="header__navbar-wrapper">
+        <li className="header__navbar-list">
+          <button
+            className="header__navbar-item"
+            onClick={() => onMenuClick('faqs')}
+          >
+            FAQ
+          </button>
+        </li>
+        <li className="header__navbar-list">
+          <button
+            className="header__navbar-item"
+            onClick={() => onMenuClick('talents')}
+          >
+            Talents
+          </button>
+        </li>
+        <li className="header__navbar-list">
+          <button
+            className="header__navbar-item"
+            onClick={() => onMenuClick('about')}
+          >
+            About Us
+          </button>
+        </li>
+      </ul>
+
+      <button onClick={() => window.location.href='https://bit.ly/TsukiakariApplicationForm'} className="header__navbar-cta">Join Us</button>
+    </>
+  )
+
+  const renderNavigation = (isMobile = false) => (
+    <nav ref={navEl} className="header__navbar">
+      {isMobile ? (
+        <div className="container">
+          {renderMenuNav()}
+          <div className="header__additional-menu">
+            <p className="header__additional-title">Contact us</p>
+
+            <div className="header__additional">
+              <a href="mailto:staff@tsukiakariacademy.com" className="header__contact-email">staff@tsukiakariacademy.com</a>
+            </div>
+          </div>
+          <div className="header__additional-menu">
+            <p className="header__additional-title">Social Media</p>
+
+            <ul className="header__additional">
+              <li className="header__additional-item">
+                <a href="https://www.facebook.com/TsukiakariAC" target="_blank" rel="noopener noreferrer">Facebook</a>
+              </li>
+              <li className="header__additional-item">
+                <a href="https://www.instagram.com/tsukiakariac/" target="_blank" rel="noopener noreferrer">Instagram</a>
+              </li>
+              <li className="header__additional-item">
+                <a href="https://twitter.com/TsukiakariAC" target="_blank" rel="noopener noreferrer">Twitter</a>
+              </li>
+              <li className="header__additional-item">
+                <a href="https://www.youtube.com/watch?v=yqWX86uT5jM" target="_blank" rel="noopener noreferrer">Youtube</a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      ) : (
+        renderMenuNav()
+      )}
+    </nav>
+  )
 
   useEffect(() => {
     mobileButtonClickHandler()
@@ -53,42 +140,14 @@ export const Header = ({ color, navigation }) => {
       <div className="container">
         <div className="header__top-decoration" style={{ backgroundColor: color  }}></div>
 
-        <div className="header__main-wrapper">
-          <h1 className="header__logo-wrapper">
-            <a href="/" className="header__logo">
-              <img src={logo} alt="Logo" />
-            </a>
-          </h1>
+        <div className="header__desktop">
+          {renderLogo()}
+          {renderNavigation()}
+        </div>
 
-          <nav className="header__navbar">
-            <ul className="header__navbar-wrapper">
-              <li className="header__navbar-list">
-                <button
-                  className="header__navbar-item"
-                  onClick={() => onMenuClick('faqs')}
-                >
-                  FAQ
-                </button>
-              </li>
-              <li className="header__navbar-list">
-                <button
-                  className="header__navbar-item"
-                  onClick={() => onMenuClick('talents')}
-                >
-                  Talents
-                </button>
-              </li>
-              <li className="header__navbar-list">
-                <button
-                  className="header__navbar-item"
-                  onClick={() => onMenuClick('about')}
-                >
-                  About Us
-                </button>
-              </li>
-            </ul>
-            <button onClick={() => window.location.href='https://bit.ly/TsukiakariApplicationForm'} className="header__navbar-cta">Join Us</button>
-          </nav>
+        <div className="header__mobile d-md-none">
+          {renderLogo()}
+          {renderNavigation(true)}
         </div>
 
         <button className="header__mobile-button" ref={mobileBtnEl}>
